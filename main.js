009edb1eb4c0d764ff6e7f002a24ef9f73407853
Brian Vuu <brianvuu@gmail.com>
2018-08-28 00:32:49 -0600

    finalized to turn in assignment

--- main.js
+++ main.js
@@ -1,49 +1,19 @@
 $(document).ready( function() {
 
-
-
     var pokemon = [
         '<img src="img/Pikachu.png">', '<img src="img/Pikachu.png">',
         '<img src="img/Charmander.png">', '<img src="img/Charmander.png">',
         '<img src="img/Bulbasaur.png">', '<img src="img/Bulbasaur.png">',
         '<img src="img/Squirtle.png">', '<img src="img/Squirtle.png">',
-
     ]
+
+    // Shuffles the pokemon array
     pokeMixed = []
     pokeMixed = pokemon.sort(function() { return 0.5 - Math.random() });
 
-    //for(i=0;i<pokemon.length;i++) {}
-
-
-    //When clicked, get card's ID
+    // When clicked, remove card cover and display pokemon 
     $('.cards').click(function(){
-        // $(this).removeClass('cover');
         cardId = $(this).attr('id');
-        $('div#'+cardId).removeClass('cover').html(pokeMixed[cardId]); 
-        // DEBUG
+        $('div#'+cardId).removeClass('cover').html(pokeMixed[cardId]);
     })
-
-
-
-
-
-//     // var test = {}
-
-//     // Shuffles the pokemons in array
-
-//     //Assign random numbers 1-8 to each pokemon in array
-//     // $(function(){
-//     //     for (var i = 0; i < pokemon.length; i++) {
-//     //     vars["test" + i] = pokemon[i]
-//     //         // DEBUG
-//     //     console.log("random: "+test);  
-//     // }})
-
-//     // Generate an array of random numbers 1-8
-//         // var randomCards = Math.floor((Math.random() * 8) + 1);
-//         // DEBUG
-//         // console.log("random number: "+randomCards);        
-
-
-
 })
